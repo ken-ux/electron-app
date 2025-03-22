d740aa0086208eef455d7d652e8e73d01d9e63a4
Kenny Nguyen <sokenns@gmail.com>
2025-03-21 19:43:47 -0700

    Add code for integrating system theme

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain } from "electron/main";
+import { app, BrowserWindow, ipcMain, nativeTheme } from "electron/main";
 import path from "node:path";
 
 const createWindow = () => {
@@ -11,10 +11,22 @@ const createWindow = () => {
   });
 
   win.loadFile("index.html");
+
+  ipcMain.handle("dark-mode:toggle", () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = "light";
+    } else {
+      nativeTheme.themeSource = "dark";
+    }
+    return nativeTheme.shouldUseDarkColors;
+  });
+
+  ipcMain.handle("dark-mode:system", () => {
+    nativeTheme.themeSource = "system";
+  });
 };
 
 app.whenReady().then(() => {
-  ipcMain.handle("ping", () => "pong");
   createWindow();
 
   app.on("activate", () => {
